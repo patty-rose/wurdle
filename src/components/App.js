14a701c08c259eb113cd3d26af171c5ef7cff5a5
Patty Rose <oteropatty@gmail.com>
2022-10-26 20:57:09 -0700

    add state placeholders

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,40 @@ import React from "react";
 import Navbar from "./Navbar";
 import GameControl from "./GameControl.js";
 import { connect } from 'react-redux';
+import PropTypes from "prop-types";
 
 class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state={
+    };
+  }
 
   render(){
     return (
       <React.Fragment>
-        <navbar />
-        <gameControl />
+        <Navbar />
+        <GameControl />
       </React.Fragment>
     );
   }
 }
 
+App.propTypes = {
+  wordDictionary : PropTypes.object,
+  playerGuesses : PropTypes.bool
+};
+
+const mapStateToProps = state => {
+  return{
+    wordDictionary : state.wordDictionary,
+    playerGuesses : state.playerGuesses
+  }
+}
+
+App = connect(mapStateToProps)(App)
 
 
 export default App;
