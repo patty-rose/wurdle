a6636b1f9c3d98758881bd858ad0507e486ded68
Patty Rose <oteropatty@gmail.com>
2022-10-30 12:58:41 -0700

    add winning alert

--- src/components/GameControl.js
+++ src/components/GameControl.js
@@ -4,6 +4,12 @@ import WordToGuessDisplay from "./WordToGuessDisplay.js";
 import GuessForm from "./GuessForm";
 
 function GameControl(props){
+  let formOrAlert = null;
+  if(!props.displayedAnswer.includes('_')){
+    formOrAlert = <div className="bg-green-100 border-t border-b border-green-500 text-green-700 px-4 py-3" role="alert"><p className="font-bold">You win!</p></div>
+  } else {
+    formOrAlert = <GuessForm whenAddLetterSubmit = {props.onAddLetterSubmit} />
+  }
   return (
     <React.Fragment>
       <h1> Game Play Area</h1>
@@ -16,7 +22,8 @@ function GameControl(props){
       </ul>
 
       <WordToGuessDisplay guessWord = {props.word} currentWordDisplayed = {props.displayedAnswer} playerMadeGuesses = {props.guessesByPlayer} />
-      <GuessForm whenAddLetterSubmit = {props.onAddLetterSubmit} />
+      {formOrAlert}
+      <h3>GUESSED LETTERS: {props.guessesByPlayer.join(" ")}</h3>
     </React.Fragment>
   );
 }
--- src/components/WordToGuessDisplay.js
+++ src/components/WordToGuessDisplay.js
@@ -7,17 +7,13 @@ function WordToGuessDisplay(props){
   return (
     <React.Fragment>
       <h1>WORD TO GUESS</h1>
-      <h2>hide or show</h2>
-      <h4>{props.currentWordDisplayed}</h4>
-      <h4>{props.playerMadeGuesses.join(" ")}</h4>
+      <h4>{props.currentWordDisplayed.join(" ")}</h4>
     </React.Fragment>
   );
 }
 
 WordToGuessDisplay.propTypes = {
-  guessWord : PropTypes.string,
-  currentWordDisplayed: PropTypes.array,
-  playerMadeGuesses : PropTypes.array
+  currentWordDisplayed: PropTypes.array
 }
 
 export default WordToGuessDisplay;
