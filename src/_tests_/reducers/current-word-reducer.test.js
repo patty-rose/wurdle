a4583b305ecf34bdc5c039547efef0689e771c73
Patty Rose <oteropatty@gmail.com>
2022-10-30 15:18:41 -0700

    add failing word check test

--- src/_tests_/reducers/current-word-reducer.test.js
+++ src/_tests_/reducers/current-word-reducer.test.js
@@ -1,9 +1,13 @@
 import currentWordReducer from '../../reducers/current-word-reducer';
 
 describe('currentWordReducer', () => {
-
+  let action;
+  
   test('should return default state if there is no action type passed into the reducer', () => {
     expect(currentWordReducer(null, {type : null})).toEqual(null);
   });
   
+  test('should return a 5 letter word from WORDS list', () => {
+    expect(currentWordReducer(null, { type:'NEW_GAME'})).toBeInstanceOf(String);
+  })
 });
