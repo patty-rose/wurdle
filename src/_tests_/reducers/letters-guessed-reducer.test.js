5c2aaad535237852034a691404e33d7abf134029
Patty Rose <oteropatty@gmail.com>
2022-10-31 20:33:57 -0700

    add failing NEW_GAME reducer test

--- src/_tests_/reducers/letters-guessed-reducer.test.js
+++ src/_tests_/reducers/letters-guessed-reducer.test.js
@@ -3,16 +3,14 @@ import lettersGuessedReducer from '../../reducers/letters-guessed-reducer.js';
 describe('lettersGuessedReducer', () => {
   let action;
 
-  const currentState = {
-
-  }
+  const currentState = ['a', 'b', 'c'] //for NEW_GAME test
 
   test('should return default state if there is no action type passed into the reducer', () => {
     expect(lettersGuessedReducer([], {type : null})).toEqual([]);
   });
 
-  test('should return state as array with "a" as single entry', () => {
-    expect(lettersGuessedReducer([], {type : 'ADD_LETTER', payload : 'a'})).toEqual(['a']);
+  test('should return state returned to default', () => {
+    expect(lettersGuessedReducer(currentState, {type : 'NEW_GAME'})).toEqual([]);
   });
 
 });
